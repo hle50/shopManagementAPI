b9ee6bfef20e54226e0ea4e3db8d038dd71ddd64
Hoa Le <hoa.le@newoceaninfosys.com>
2018-04-18 16:33:24 +0700

    update API product

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -26,7 +26,39 @@ exports.createProduct = (req, res) => {
 
 // get Product by Id
 
+exports.getProductById = (req, res) =>{
+  const _id = req.params.id;
+  Product.findById(_id)
+    .exec()
+    .then(result =>{
+      res.status(200).json({
+        result
+      })
+    })
+    .catch(error =>{
+      res.status(400).json({
+        error
+      })
+    })
+}
+
 // get all product
+
+exports.getAll = (req, res) =>{
+  const user =req.userData.userId;
+  Product.find({user})
+    .exec()
+    .then(result =>{
+      res.status(200).json({
+        result
+      })
+    })
+    .catch(error =>{
+      res.status(500).json({
+        error
+      })
+    })
+};
 
 // update product
 
--- api/routes/product.js
+++ api/routes/product.js
@@ -4,5 +4,7 @@ const ProductController = require('../controllers/product');
 const checkAuth = require('../middlewares/check-Auth');
 
 router.post('/create', checkAuth, ProductController.createProduct);
+router.get('/:id', checkAuth, ProductController.getProductById);
+router.get('', checkAuth, ProductController.getAll);
 
 module.exports = router;
